246e2cc4bbe445af97fe717db76fc1adbda11a89
Barry Song <21cnbao@gmail.com>
2014-06-30 08:26:57 +0800

    drivers: cleanup export_symb and book
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- kernel/drivers/param/book.c
+++ kernel/drivers/param/book.c
@@ -12,20 +12,20 @@
 static char *book_name = "dissecting Linux Device Driver";
 module_param(book_name, charp, S_IRUGO);
 
-static int num = 4000;
-module_param(num, int, S_IRUGO);
+static int book_num = 4000;
+module_param(book_num, int, S_IRUGO);
 
 static int __init book_init(void)
 {
-	printk(KERN_INFO " book name:%s\n",book_name);
-	printk(KERN_INFO " book num:%d\n",num);
+	printk(KERN_INFO "book name:%s\n", book_name);
+	printk(KERN_INFO "book num:%d\n", book_num);
 	return 0;
 }
 module_init(book_init);
 
 static void __exit book_exit(void)
 {
-	printk(KERN_INFO " Book module exit\n ");
+	printk(KERN_INFO "book module exit\n ");
 }
 module_exit(book_exit);
 
