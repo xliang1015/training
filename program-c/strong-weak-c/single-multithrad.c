fa8cfc43714c6cf18aaafee7f00222a6e9db3141
Barry Song <21cnbao@gmail.com>
2015-06-26 07:51:21 +0800

    use __attribute__ ((weak))
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- program-c/strong-weak-c/single-multithrad.c
+++ program-c/strong-weak-c/single-multithrad.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 int pthread_create( pthread_t*, const pthread_attr_t*,
-void* (*)(void*), void*) __attribute__ ((weakref));
+void* (*)(void*), void*) __attribute__ ((weak));
 int main()
 {
     if(pthread_create)
