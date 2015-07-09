76a69dc6faed2e79efbb75f2a60276cba7d5c09b
Barry Song <21cnbao@gmail.com>
2015-07-10 06:50:48 +0800

    fix malloopt, check the ret of mlockall
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- preempt-rt/mallopt.c
+++ preempt-rt/mallopt.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 	unsigned char *buffer;
 	int i;
 
-	if (mlockall(MCL_CURRENT | MCL_FUTURE))
+	if (!mlockall(MCL_CURRENT | MCL_FUTURE))
 		mallopt(M_TRIM_THRESHOLD, -1);
 	mallopt(M_MMAP_MAX, 0);
 
@@ -20,10 +20,11 @@ int main(int argc, char *argv[])
 	 * Touch each page in this piece of memory to get it
 	 * mapped into RAM
 	 */
-	for (i = 0; i < SOMESIZE; i += page_size)
+	for (i = 0; i < SOMESIZE; i += 4 * 1024)
 		buffer[i] = 0;
 	free(buffer);
 	/* <do your RT-thing> */
 
+	while(1);
 	return 0;
 }
