cfeaba6e06d65545b252abe3f01e7242bc501d9b
Barry Song <21cnbao@gmail.com>
2017-04-16 19:21:55 +0800

    add trustzone SW
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- arm/safeg-0.4-qemu-14dec2012/prepare_env.sh
+++
@@ -1,4 +0,0 @@
-arm-none-eabi-gcc --version >/dev/null 2>&1 || { echo "SafeG requires arm-none-eabi-gcc (arm-2012.03-56-arm-none-eabi.bin) cross-compiler to be installed."; exit 1;}
-sudo apt-get update || { echo "SafeG requires Internet to download source code."; exit 1;}
-sudo apt-get install build-essential libsdl-dev libboost-regex-dev libboost-system-dev libboost-filesystem-dev libboost-program-options-dev
-
--- arm/safeg-0.4-qemu-14dec2012/run_qemu.sh
+++
@@ -1,7 +0,0 @@
-cp fmp-app/fmp.bin build/t.bin
-cp safeg/kernel-wrapper/nt-linux.bin build/nt.bin
-cp safeg/safeg.bin build/
-./build/qemu-trustzone-devel/arm-softmmu/qemu-system-arm -cpu cortex-a9 -M vexpress-a9 -smp 1 -serial vc:80Cx40C -serial vc:80Cx40C -serial vc:80Cx40C -serial vc:80Cx40C -no-reboot -m 1024M -tftp build/ -drive if=sd,cache=writeback,file=prebuilt/rootfs.ext2 -kernel build/u-boot-linaro-stable-c5d34d0/u-boot
-
-
-
