ccf478e4bdfe6156e592207b34ab0c5cedee4a39
Barry Song <21cnbao@gmail.com>
2017-08-10 22:11:03 +0800

    indent -linux for shell.cpp
    Signed-off-by: Barry Song <21cnbao@gmail.com>

--- c++/shell.cpp
+++ c++/shell.cpp
@@ -12,171 +12,160 @@ using namespace std;
 
 //////////////////////////////////////////////////////////////////////////
 
-typedef int (*CCmd)(int argc, char* argv[]);
+typedef int (*CCmd) (int argc, char *argv[]);
 
-typedef struct{
-	char*	Name;
-	CCmd	CmdTable;
-}CCmdTable;
+typedef struct {
+	char *Name;
+	CCmd CmdTable;
+} CCmdTable;
 
 //////////////////////////////////////////////////////////////////////////
 
-int Cmd1(int argc, char* argv[]);
-int Cmd2(int argc, char* argv[]);
-int InitModel(int argc, char* argv[]);
-
+int Cmd1(int argc, char *argv[]);
+int Cmd2(int argc, char *argv[]);
+int InitModel(int argc, char *argv[]);
 
 //////////////////////////////////////////////////////////////////////////
 
 // 命令列表
 
-CCmdTable CmdTable[SHELL_CMD_SPACE]=
-{
+CCmdTable CmdTable[SHELL_CMD_SPACE] = {
 	{"cmd1", Cmd1},
 	{"cmd2", Cmd2},
 };
 
 //////////////////////////////////////////////////////////////////////////
 
-int main(int argc, char* argv[])
-
+int main(int argc, char *argv[])
 {
 
-while(1)
+	while (1)
+	{
 
-{
+		char str[128];
 
-char str[128];
+		// 处理输入错误
 
-// 处理输入错误
+		if (cin.fail())
+		{
 
-if (cin.fail())
+			cin.clear();
 
-{
+		}
+		// 获得输入命令
 
-cin.clear();
+		cout << "Shell:>";
 
-}
+		cin >> str;
 
-// 获得输入命令
+		// 如果命令为退出命令，在单片机等程序中，不应退出
 
-cout<<"Shell:>";
+		if (0 == strcmp(str, "quit"))
+		{
 
-cin>>str;
+			break;
 
-// 如果命令为退出命令，在单片机等程序中，不应退出
+		}
+		// 搜索表中的所有命令，找出与输入命令匹配的并执行
 
-if (0==strcmp(str, "quit"))
+		for (int i = 0; i < SHELL_CMD_SPACE; i++)
+		{
 
-{
+			// 无命令则提前返回
 
-break;
+			if (CmdTable[i].Name == NULL)
+				continue;
 
-}
+			// 如果命令正确
 
-// 搜索表中的所有命令，找出与输入命令匹配的并执行
+			if (0 == strcmp(str, CmdTable[i].Name))
+			{
 
-for (int i=0; i<SHELL_CMD_SPACE; i++)
+				// 获取输入参数
 
-{
+				cin.get(str, 128, '\n');
 
-// 无命令则提前返回
+				fflush(stdin);	// 及时清空输入流
 
-if (CmdTable[i].Name==NULL) continue;
+				char *t_argv[SHELL_ARGV_SPACE];
 
-// 如果命令正确
+				int j = 0;
 
-if (0==strcmp(str, CmdTable[i].Name))
+				t_argv[0] = strtok(str, " ");	// 分割输入参数
 
-{
+				while (t_argv[j++] != NULL)
+				{
 
-// 获取输入参数
+					t_argv[j] = strtok(NULL, " ");
 
-cin.get(str, 128, '\n');
+				}
 
-fflush(stdin);	// 及时清空输入流
+				try	// 执行命令
+				{
 
-char* t_argv[SHELL_ARGV_SPACE];
+					int t_return =
+					    CmdTable[i].CmdTable(j - 1, t_argv);
 
-int j=0;
+					cout << "命令退出，返回[" <<
+					    t_return << "]" << endl;
 
-t_argv[0] = strtok (str," ");	// 分割输入参数
+				}
 
-while(t_argv[j++]!=NULL)
+				catch( ...)	// 捕获异常，维持稳定性
+				{
 
-{
+					cout <<
+					    "捕获到异常，返回主程序..."
+					    << endl;
 
-t_argv[j] = strtok(NULL," ");
+				}
 
-}
+				break;
 
-try	// 执行命令
+			}
 
-{
+		}
 
-int t_return = CmdTable[i].CmdTable(j-1, t_argv);
+		// 命令错误提示
 
-cout<<"命令退出，返回["<<t_return<<"]"<<endl;
+		if (i == SHELL_CMD_SPACE)
+		{
 
-}
+			fflush(stdin);
 
-catch (...)	// 捕获异常，维持稳定性
+			cout << "未搜索到命令：" << str << endl;
 
-{
+			// 添加默认操作
 
-cout<<"捕获到异常，返回主程序..."<<endl;
+		}
 
-}
+	}
 
-break;
-
-}
-
-}
-
-// 命令错误提示
-
-if (i==SHELL_CMD_SPACE)
-
-{
-
-fflush(stdin);
-
-cout<<"未搜索到命令："<<str<<endl;
-
-// 添加默认操作
-
-}
-
-}
-
-return 0;
+	return 0;
 
 }
 
 //////////////////////////////////////////////////////////////////////////
 
-int Cmd1(int argc, char* argv[])
+int Cmd1(int argc, char *argv[])
 {
-	cout<<"Cmd1"<<endl;
+	cout << "Cmd1" << endl;
 
-	cout<<argc<<endl;
+	cout << argc << endl;
 
-	for (int i=0; i<argc; i++)
-
+	for (int i = 0; i < argc; i++)
 	{
 
-		cout<<argv[i]<<endl;
+		cout << argv[i] << endl;
 
 	}
 
 	return 1;
 }
 
-int Cmd2(int argc, char* argv[])
+int Cmd2(int argc, char *argv[])
 {
-	cout<<"Cmd2"<<endl;
+	cout << "Cmd2" << endl;
 
 	return 2;
 }
-
